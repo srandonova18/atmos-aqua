31215ffbc8c68cfcc65981860d94a144027cc40b
BNSavov19 <BNSavov19@codingburgas.bg>
2021-06-28 00:38:14 +0300

    Add login form with fetch

--- app/public/javascripts/loginManager.js
+++ app/public/javascripts/loginManager.js
@@ -1,5 +1,18 @@
 const loginButton = document.querySelector('.login-button');
+const loginForm = document.querySelector('.login-form');
 
 loginButton.addEventListener('click', () => {
     console.log("login yes");
+});
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let obj ={
+        email: document.querySelector('#login-email').value,
+        password: document.querySelector('#login-password').value
+    };
+
+    fetch('/api/login', {method: 'POST', body: obj});
+
 });
