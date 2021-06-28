56bc52712a62142f2d55787e44dad0674fa2ab04
BNSavov19 <BNSavov19@codingburgas.bg>
2021-06-28 22:37:18 +0300

    Add login form enter animation

--- app/public/javascripts/loginManager.js
+++ app/public/javascripts/loginManager.js
@@ -1,5 +1,6 @@
 const loginButton = document.querySelector('.login-button');
 const loginForm = document.querySelector('.login-form');
+const loginFormBox = document.querySelector('.form-box');
 const loginFormContainer = document.querySelector('.form-container');
 let formIsVisible = false;
 
@@ -8,9 +9,13 @@ loginButton.addEventListener('click', () => {
     {
         formIsVisible = true;
         loginFormContainer.style.display = 'block';
+        loginFormContainer.classList.add('form-bg-blur-animation');
+        loginFormBox.classList.add('form-enter-animation');
     } else if(formIsVisible) {
         formIsVisible = false;
         loginFormContainer.style.display = 'none';
+        loginFormContainer.classList.remove('form-bg-blur-animation');
+        loginFormBox.classList.remove('form-enter-animation');
     }
 });
 
