29ddb0fecbd02026e2e4c4f895b2de812bc5a3ae
Борис Николаев Савов <BNSavov19@codingburgas.bg>
2021-06-29 13:18:28 +0300

    Add register port functionality

--- app/public/javascripts/createPort.js
+++ app/public/javascripts/createPort.js
@@ -1,9 +1,20 @@
 const createButton = document.querySelector('.create-port-button');
+const finishSelButton = document.querySelector('.finish-selection-button');
 const mapElement = document.querySelector('#map');
 const instructions = document.querySelector('.instructions');
+const portFormContainer = document.querySelector('.form-container');
+const portFormBox = document.querySelector('.form-box');
 
 createButton.addEventListener('click', () => {
     mapElement.style.filter = 'blur(0px)';
     createButton.style.display = 'none';
     instructions.style.display = 'block';
+    finishSelButton.style.display = 'block';
+});
+
+finishSelButton.addEventListener('click', () => {
+    portFormContainer.style.display = 'block';
+    portFormContainer.classList.add('form-bg-blur-animation');
+    portFormBox.classList.add('form-enter-animation');
+    finishSelButton.style.display = 'none';
 });
