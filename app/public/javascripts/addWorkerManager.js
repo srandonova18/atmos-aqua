67c9c23a92d105e342be9ef07374dd421f19b889
Борис Николаев Савов <BNSavov19@codingburgas.bg>
2021-06-29 14:37:51 +0300

    Last commit

--- app/public/javascripts/addWorkerManager.js
+++ app/public/javascripts/addWorkerManager.js
@@ -1,9 +1,16 @@
 const workerButton = document.querySelector('.add-worker-button');
 const portFormContainer = document.querySelector('.form-container');
 const portFormBox = document.querySelector('.form-box');
+const workerPassword = document.querySelector('#worker-pass');
+const workerForm = document.querySelector('.worker-form');
 
 workerButton.addEventListener('click', () => {
     portFormContainer.style.display = 'block';
     portFormContainer.classList.add('form-bg-blur-animation');
     portFormBox.classList.add('form-enter-animation');
 });
+
+// workerForm.onSubmit = function(e) {
+//     workerPassword.value = new Hashes.SHA256().hex(workerPassword.value);
+//     return true;
+// }
--- app/public/javascripts/createPort.js
+++ app/public/javascripts/createPort.js
@@ -4,6 +4,18 @@ const mapElement = document.querySelector('#map');
 const instructions = document.querySelector('.instructions');
 const portFormContainer = document.querySelector('.form-container');
 const portFormBox = document.querySelector('.form-box');
+const form = document.querySelector('.port-form');
+const pass = document.querySelector('#pass');
+
+console.log(form);
+console.log(pass);
+
+// form.onSubmit = function(e) {
+//     pass.value = new Hashes.SHA256().hex(pass.value);
+//     return true;
+// }
+
+console.log(new Hashes.SHA256().hex('admin'));
 
 createButton.addEventListener('click', () => {
     mapElement.style.filter = 'blur(0px)';
@@ -18,3 +30,4 @@ finishSelButton.addEventListener('click', () => {
     portFormBox.classList.add('form-enter-animation');
     finishSelButton.style.display = 'none';
 });
+
